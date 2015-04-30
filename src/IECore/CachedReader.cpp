056a2cb37f4a8a41d2e9844a10a0acc185b56396
unknown <alex.fuller@SETH05.melb.il.com.au>
2015-04-30 12:11:05 +1000

    Revert "Typecasts (fixes MSVC compile errors)."
    This reverts commit 3f8ea6583b64a682e674d18da0f4d51615b9c8e1.

--- src/IECore/CachedReader.cpp
+++ src/IECore/CachedReader.cpp
@@ -207,7 +207,7 @@ bool CachedReader::cached( const std::string &file ) const
 		}
 	}
 
-	return (bool)m_data->m_cache.get( PARAM(file), MemberData::Cache::NullIfMissing );
+	return m_data->m_cache.get( PARAM(file), MemberData::Cache::NullIfMissing );
 }
 
 void CachedReader::clear()
--- src/IECore/ImageWriter.cpp
+++ src/IECore/ImageWriter.cpp
@@ -118,7 +118,7 @@ const BoolParameter * ImageWriter::rawChannelsParameter() const
 
 bool ImageWriter::canWrite( ConstObjectPtr image, const string &fileName )
 {
-	return (bool)runTimeCast<const ImagePrimitive>( image );
+	return runTimeCast<const ImagePrimitive>( image );
 }
 
 /// get the user-requested channel names
