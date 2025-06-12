1d59c3864e47c56064693d3b0a8d87aeca4e4014
Alex Fuller <alex.fuller@flyingbark.com.au>
2025-06-13 06:49:51 +1000

    IECoreUSD : Re-arrange writeAttribute so that __bindMaterial doesn't get written out with a purpose set

--- contrib/IECoreUSD/src/IECoreUSD/USDScene.cpp
+++ contrib/IECoreUSD/src/IECoreUSD/USDScene.cpp
@@ -1464,6 +1464,14 @@ void USDScene::writeAttribute( const SceneInterface::Name &name, const Object *a
 			}
 		}
 	}
+	else if( boost::starts_with( name.string(), "__materialBind" ) )
+	{
+		if( const IECore::StringData *stringData = runTimeCast<const StringData>( attribute ) )
+		{
+			const auto &[output, purpose] = materialOutputAndPurpose( name.string() );
+			m_materialBinds[purpose] = pxr::SdfPath( stringData->readable() );
+		}
+	}
 	else if( name.string().find( ':' ) != std::string::npos )
 	{
 		if( const Data *data = runTimeCast<const Data>( attribute ) )
@@ -1484,14 +1492,6 @@ void USDScene::writeAttribute( const SceneInterface::Name &name, const Object *a
 				);
 				newAttribute.Set( DataAlgo::toUSD( data ), m_root->timeCode( time ) );
 			}
-		}
-	}
-	else if( boost::starts_with( name.string(), "__materialBind" ) )
-	{
-		if( const IECore::StringData *stringData = runTimeCast<const StringData>( attribute ) )
-		{
-			const auto &[output, purpose] = materialOutputAndPurpose( name.string() );
-			m_materialBinds[purpose] = pxr::SdfPath( stringData->readable() );
 		}
 	}
 }
